d71bd5cf2ab62873e568512e1bed18f0eb854c3a
nauosika <nauosikaforwork@gmail.com>
2021-09-16 22:18:20 +0800

    reset routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :articles
-      resources :sessions, only: [:create, :destroy]
+      post "/signup", to: "auth#signup"
+      post "/login", to: "auth#login"
+      post "/logout", to: "auth#logout"
     end
   end
 end
